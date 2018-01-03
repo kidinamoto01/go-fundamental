29d9097b6d29081b5ff063a4143c6805891f6540
kidinamoto01 <kidinamoto@qq.com>
2018-01-03 17:00:16 +0800

    add switch demo

--- lecture6.go
+++ lecture6.go
@@ -21,10 +21,62 @@ func main() {
 
 	//判断语句if
 	//条件表达式没有括号
-	a := -1
-	if a := 1; a > 0 {
+	m := -1
+	if m := 1; m > 0 {
 
 		//a的作用范围只在if语句内
-		fmt.Println("a>0")
+		fmt.Println("m>0")
 	}
+	fmt.Println(m)
+	//for实现多个关键字的形式
+
+	b := 1
+	//无限循环
+	for {
+
+		b++
+		if b > 3 {
+			break
+		}
+
+		fmt.Println(b)
+	}
+
+	fmt.Println("over")
+
+	//与java相类似的for循环形式
+	s := "string"
+	for i := 0; i < len(s); i++ {
+
+		fmt.Println(i)
+	}
+
+	//单一判断语句
+	i := 0
+	for i < len(s) {
+		i++
+		fmt.Println(i)
+	}
+
+	//switch 语句的多种形式
+	switch i {
+	case 0:
+		fmt.Println("i=0")
+	case 1:
+		fmt.Println("i=1")
+	default:
+		fmt.Println("i>1")
+
+	}
+
+	//switch后没有条件表达式
+	switch {
+	case i > 1:
+		fmt.Println("i>1")
+		fallthrough //不执行break，继续判断
+	case i > 2:
+		fmt.Println("i>2")
+
+	}
+
 }
